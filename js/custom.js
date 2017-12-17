16e4f6799b351186fa2476023aefd8e16bbcb812
Aruna <aruaruna1297@gmail.com>
2017-12-17 23:04:57 +0530

    schedule,mega event and workshop

--- js/custom.js
+++ js/custom.js
@@ -91,6 +91,20 @@ $(document).ready(function() {
     });
   });
 
+ /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-workshop").owlCarousel({
+      autoPlay: 6000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
+ 
 
 
   /* Back top
